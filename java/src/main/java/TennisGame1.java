4fc09d3553f5767b126522ce67924ce53bcd2dd6
MatteoStromieri <matteostromieri@outlook.it>
2023-11-22 19:31:01 +0100

    extracted isOneScoreGreaterEqual4() and getPoints()

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -19,62 +19,84 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
+        String score;
         int tempScore=0;
-        if (score1EqualsScore2())
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
+        if (isTie())
+            score = getTieScore();
+        else if (isOneScoreGreaterEqual4())
+            score = getAdvantageScore();
         else
         {
-            for (int i=1; i<3; i++)
+            score = getPoints();
+        }
+        return score;
+    }
+
+    private String getPoints() {
+        String score="";
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1)
+                tempScore = m_score1;
+            else {
+                score +="-";
+                tempScore = m_score2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
             }
         }
         return score;
     }
 
-    private boolean score1EqualsScore2() {
+    private String getAdvantageScore() {
+        String score;
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private boolean isOneScoreGreaterEqual4() {
+        return m_score1 >= 4 || m_score2 >= 4;
+    }
+
+    private boolean isTie() {
         return m_score1 == m_score2;
+    }
+
+    private String getTieScore() {
+        String score;
+        switch (m_score1)
+        {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
+        }
+        return score;
     }
 }
